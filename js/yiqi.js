7e8a082ab769603eadd71eab3e7267d9e7702971
liuyang111bug <62432943+liuyang111bug@users.noreply.github.com>
2020-03-24 10:08:46 +0800

    Add files via upload

---
+++ js/yiqi.js
@@ -0,0 +1,34 @@
+﻿// JavaScript Document
+var Dianji=0;
+window.onload=function(){
+	var buhao = document.getElementById("buhao");
+	var hao = document.getElementById("hao");
+	buhao.onclick=function(){
+		Dianji++;
+		if(Dianji==1){
+	   			alert("转街 ");
+	   }else if(Dianji==2){
+		   		alert("喝抹茶味奶茶");
+		   		
+	   }else if(Dianji==3){
+		   		alert("超市买你喜欢的小零食");
+		   		
+	   }else if(Dianji==4){
+		   		alert("做你想做的事");
+		   		
+	  }else if(Dianji==5){
+		   		alert("好不好");
+	  }else if(Dianji==6){
+		   		alert("好不好");
+	  }else if(Dianji==7){
+		   		alert("好不好");
+		  		Dianji=1;
+	  }
+
+	}
+	hao.onclick=function(){
+		alert("我去找你，等着吖");
+	}
+	
+
+}
